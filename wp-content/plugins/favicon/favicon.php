8e3a22693c644c208f45ff2d77d11d8f4e2f7052
georgesngandeu <georgesngandeu@gmail.com>
2021-06-08 16:11:19 +0100

    favicon example

---
+++ wp-content/plugins/favicon/favicon.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  Plugin Name: Favicon
+  Plugin URI:
+  Description: Adding favion
+  Author: Georges Ngandeu
+  Version: 1.0
+  Author URI: https://github.com/Georges-Ngandeu
+ */
+
+add_action( 'wp_head', 'page_header_favicon' );
+
+function page_header_favicon() {
+	$site_icon_url = get_site_icon_url();
+	if ( !empty( $site_icon_url ) ) {
+		wp_site_icon();
+	} else {
+		$icon_url = plugins_url( 'favicon.ico', __FILE__ );
+?>
+
+    <link rel="shortcut icon" href="<?php echo $icon_url; ?>" />
+ <?php  
+	}
+}
+
+
