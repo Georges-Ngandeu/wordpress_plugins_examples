3776c1f00a3cd8d9cc657c09cd47a47be393bfa0
georgesngandeu <georgesngandeu@gmail.com>
2021-06-08 15:38:07 +0100

    created simple plugin header

---
+++ wp-content/plugins/plugin-header/plugin-header.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name:	Plugin Header
+Plugin URI:
+Description:	Declares a plugin that will be visible in the WordPress admin interface
+Version:		1.0
+Author:			Georges Ngandeu
+Author URI:		https://github.com/Georges-Ngandeu
+License:		GPLv2
+*/
